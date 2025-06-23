e5f6ab56d14180079d2b86ec79a4ade2ed634c90
shrutiagarwal21 <khushiagarwal0204@gmail.com>
2025-06-23 19:24:02 +0530

    logout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,23 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
+  const router = useRouter();
+  const onLogout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
   return (
     <div>
       <h1>Profile</h1>
       <hr />
+      <button onClick={onLogout}>Logout</button>
     </div>
   );
 }
