444c1d71ea09ab5a08f0d019322a765a8223803e
shrutiagarwal21 <khushiagarwal0204@gmail.com>
2025-06-23 22:53:55 +0530

    middle-ware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 import axios from "axios";
 import Link from "next/link";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("nothing");
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   const onLogout = async () => {
     try {
       await axios.get("/api/users/logout");
@@ -16,8 +25,17 @@ export default function ProfilePage() {
   return (
     <div>
       <h1>Profile</h1>
+      <h2>
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <hr />
       <button onClick={onLogout}>Logout</button>
+      <hr />
+      <button onClick={getUserDetails}>get user detail</button>
     </div>
   );
 }
